92d1ff75a973da7ec390581c72c32fe53572db1b
vorobyevdmitriy <vip.niznay@gmail.com>
2026-06-03 23:47:48 +0300

    refactor: use dedicated CMA-CGM datamart input

--- company_status_mapper.py
+++ company_status_mapper.py
@@ -1,6 +1,6 @@
 from status_processing.cma_cgm.mapper import CmaCgmMapper
 from status_processing.config.paths import (
-    DATAMART,
+    DATAMART_CMA_CGM,
     MAPPED_DATAMART_CMA_CGM,
 )
 from status_processing.mapping_runner import run_company_mapping
@@ -9,7 +9,7 @@ from status_processing.mapping_runner import run_company_mapping
 def main():
     run_company_mapping(
         company="CMA-CGM",
-        input_path=str(DATAMART),
+        input_path=str(DATAMART_CMA_CGM),
         output_events=None,
         output_datamart_like=str(MAPPED_DATAMART_CMA_CGM),
         output_chains=None,
